b04f0a4cc9f1402cc54b14dbcd342635331be979
Zuyeonn <162269448+Zuyeonn@users.noreply.github.com>
2024-11-26 18:54:57 +0900

    7주차 실습

---
+++ app/src/main/java/com/example/umc_study7/ProfileDao.kt
@@ -0,0 +1,23 @@
+package com.example.umc_study7
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+//Data Access Object
+@Dao
+interface ProfileDao {
+    @Insert     // 사로운 프로필 추가
+    fun insert(profile: Profile)
+
+    @Update     // 기존 프로필 수정
+    fun update(profile: Profile)
+
+    @Delete     // 프로필 삭제
+    fun delete(profile:Profile)
+
+    @Query("SELECT * FROM Profile")     // 모든 프로필의 데이터 조회
+    fun getAll(): List<Profile>
+}
